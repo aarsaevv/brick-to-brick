6e174da86f66022540696a666c2d1cabb02436d7
Evgeny Arsaev <vltby000@gmail.com>
2025-08-27 16:34:48 +0700

    init(app): add necessary deps and small cfg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import path from 'path';
+
+const srcDir = path.resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue'],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': srcDir,
+    },
+  },
+});
